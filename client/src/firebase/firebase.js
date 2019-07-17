f535f8d5f8214d12ca49518b8c04aa6841a8da5c
Emma-Barash <emma.barash.dev@gmail.com>
2019-07-17 10:32:50 -0600

    react environment for firebase keys

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,5 +1,20 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/storage';
 
-var config = 
+var config = {
+    apiKey: REACT_APP_APIKEY,
+    authDomain: REACT_APP_AUTHDOMAIN,
+    databaseURL: REACT_APP_DBURL,
+    projectId: REACT_APP_PID,
+    storageBucket: REACT_APP_STORAGEB,
+    messagingSenderId: REACT_APP_MESSAGINGSENDERID,
+    appId: REACT_APP_APPID
+  };
 
 firbase.initializeApp(config);
+
+const storage = firebase.storage();
+
+export {
+    storage, firbase as default
+}
